8e0b8e3df1e95b7f04fafdc17ef04fd18f6b041d
Surajsaw08 <143094982+Surajsaw08@users.noreply.github.com>
2025-08-20 16:59:00 +0530

    correct couponverification api call

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -47,7 +47,7 @@ export default function Checkout() {
   // Apply coupon
   const applyCoupon = async () => {
     try {
-      const res = await fetch("http://localhost:3000/api/couponVerification", {
+      const res = await fetch("/api/couponVerification", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
